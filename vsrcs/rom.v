c702c61b9b4b5be353815e65397f1d543fa41e25
JasonHuo <huotiansheng@126.com>
2026-03-09 21:55:42 +0800

    完善了rom模块

--- vsrcs/rom.v
+++ vsrcs/rom.v
@@ -1,10 +1,11 @@
 module rom(
   input [31: 0] romaddr_in,
-  output reg [31: 0] romdata_out
+  input clk,
+  output [31: 0] romdata_out
 );
 
-  reg [31: 0] rom_reg [0: 4294967295];
-  always@(pc_in)
-	romdata_out = rom_reg[romaddr_in >> 2];
+  reg [31: 0] rom_reg [0: 1023];
+
+  assign data = rom_reg[romaddr_in[31: 2]];
 
 endmodule
