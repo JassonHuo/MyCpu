b41fdb4ceaa2afa8fdc9145eada27ac5211b1f56
JasonHuo <huotiansheng@126.com>
2026-03-11 15:26:39 +0800

    完成了部分alu的功能，初步确定了ALU的结构，但是对于位移计算仍存在一定问题需要解决

--- vsrcs/alu.v
+++ vsrcs/alu.v
@@ -1,19 +1,29 @@
 module alu(
   input [31: 0] x,
   input [31: 0] y,
-  input [: 0] sel,
-  output [31: 0] z,
-
+  input [3: 0] sel,
+  output reg [31: 0] z,
+  output overflow,
+  output zero
 );
+
+  parameter ADD = 4'd0, SUB = 4'd1, AND = 4'd2, OR = 4'd3, XOR = 4'd4, SLL = 4'd5, SRL = 4'd6, SRA = 4'd7;
+
+  assign zero = (z == 0);
 
   always@(*)begin
 	case(sel)
-	  4'b0000: z = x + y;
-	  4'b0001: z = x - y;
-	  4'b0010: z = x & y;
-	  4'b0011: z = x | y;
-	  4'b0100: z = x ^ y;
-	  4'b0101: z = 
+	  ADD: z <= x + y;
+	  SUB: z <= x - y;
+	  AND: z <= x & y;
+	  OR: z <= x | y;
+	  XOR: z <= x ^ y;
+	  SLL: z <= ;
+	  SRL: z <= ;
+	  SRA: z <= ;
+	  default: z <= 0;
 
+	endcase
+  end
 
 endmodule
