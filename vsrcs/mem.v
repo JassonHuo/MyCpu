d09f0eb0b8bc8cf9e9fbf995f6caea7d787958e7
JasonHuo <huotiansheng@126.com>
2026-03-12 16:26:38 +0800

    创建了部分mem模块端口

--- vsrcs/mem.v
+++ vsrcs/mem.v
@@ -16,9 +16,22 @@ module mem(
   output reg [31: 0] pc_out,
   output reg [31: 0] inst_out,
   output reg [31: 0] memaddr_out,
-
+  output reg bt0en_out,
+  output reg bt1en_out,
+  output reg bt2en_out,
+  output reg bt3en_out,
+  output reg readen_out,
+  output reg [31: 0] ramaddr_out,
+  input [31: 0] ramreadnum_in
 );
 
-
+  assign pc_out = pc_in;
+  assign inst_out = inst_in;
+  assign bt0en_out = bt0en_in;
+  assign bt1en_out = bt1en_in;
+  assign bt2en_out = bt2en_in;
+  assign bt3en_out = bt3en_in;
+  assign readen_out = readen_in;
+  assign ramaddr_out = ramaddr_in;  
 
 endmodule
