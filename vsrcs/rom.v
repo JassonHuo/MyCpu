8725c10a5e9ae2dfe58a079fef1bf7842cf38d65
JasonHuo <huotiansheng@126.com>
2026-03-14 13:17:29 +0800

    添加了读取外部程序的功能

--- vsrcs/rom.v
+++ vsrcs/rom.v
@@ -5,6 +5,10 @@ module rom(
   output [31: 0] romdata_out
 );
 
+  initial begin
+	$readmemb("sum.bin", rom_reg;
+  end
+
   reg [31: 0] rom_reg [0: 1023];
 
   assign romdata_out = rom_reg[romaddr_in[11: 2]];   //地址宽度需随情况修改
