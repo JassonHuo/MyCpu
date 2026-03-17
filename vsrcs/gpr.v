29c274b28089339c3b54126882fd7d6b130bd14b
JasonHuo <huotiansheng@126.com>
2026-03-17 18:51:47 +0800

    在gpr添加了数据旁路解决部分冒险问题

--- vsrcs/gpr.v
+++ vsrcs/gpr.v
@@ -15,9 +15,9 @@ module gpr(
 
   reg [31: 0] gpr_reg [0: 31];
 
-  assign readdata1_out = (readaddr1_in == 0 ? 32'b0: gpr_reg[readaddr1_in]);
-  assign readdata2_out = (readaddr2_in == 0 ? 32'b0: gpr_reg[readaddr2_in]);
-
+  assign readdata1_out = (wriaddr_in != 0 && readaddr1_in == wriaddr_in && wen) ? (wridata_in): (readaddr1_in == 0 ? 32'b0: gpr_reg[readaddr1_in]);
+  assign readdata2_out = (wriaddr_in != 0 && readaddr2_in == wriaddr_in && wen) ? (wridata_in): (readaddr2_in == 0 ? 32'b0: gpr_reg[readaddr2_in]);
+  
   always@(posedge clk)begin
 	if(wen == 1 && wriaddr_in != 0)
 		gpr_reg[wriaddr_in] <= wridata_in;
